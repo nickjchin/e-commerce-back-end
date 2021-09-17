43fe5f951b662ab733815bd0e28cc9ff7891b06d
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-09-16 23:33:38 -0400

    got get all tags to work

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,12 @@ const { Tag, Product, ProductTag } = require("../../models");
 router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({ include: [{ model: Product, through: ProductTag }] });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/:id", async (req, res) => {
