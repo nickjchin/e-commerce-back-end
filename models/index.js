069901b7e3544b7890c3ca986a2104001d7c0524
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-09-16 20:34:10 -0400

    fixed issue where there was a comma inside a quote

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ Category.hasMany(Product, {
   foreignKey: "category_id",
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id," });
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { through: ProductTag });
 
